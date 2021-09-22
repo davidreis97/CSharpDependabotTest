1fa6a8692d5444ef1e0943538de0e1894868c715
David Reis <dreis@semasio.com>
2021-09-22 11:23:31 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CSharpDependabotTest
+{
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("hey");
+        }
+    }
+}
