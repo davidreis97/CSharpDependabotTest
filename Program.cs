40df42e6af11c1acb4664c0ce65a2a8ce2f02ce3
David Reis <dreis@semasio.com>
2021-09-22 11:39:19 +0100

    Add nlog dependency

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using NLog;
 
 namespace CSharpDependabotTest
 {
     public class Program{
         public static void Main(string[] args){
+            LogManager.GetCurrentClassLogger().Info("Hey");
             Console.WriteLine("hey");
         }
     }
